a2ca681d4c4b23c2b67aa21239a058c75cd8ef01
Rolando Valdenegro <r.valdenegrof@gmail.com>
2021-04-12 15:06:46 -0400

    Se intenta dejar navbar transparente, que cambie de color cuando salga de su posiciòn a bg-dark

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(function () {
+    $(window).scroll(function () { 
+        if($("#menu").offset().top > 100){
+            $("#menu").addClass("bg-dark");
+        } else {
+            $("#menu").removeClass("bg-dark");
+        }
+    });
+});
+ 
