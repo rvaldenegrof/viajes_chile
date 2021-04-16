e322bea6f710e4230292f7205d8379c29d41d993
rvaldenegrof <r.valdenegrof@gmail.com>
2021-04-16 13:13:30 -0400

    Se agregan tooltips a la sección QUIENES SOMOS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 $(function () {
-
     /****SCROLL******/
     $(window).scroll(function(){
         $('nav').toggleClass('scrolled', $(this).scrollTop() > 600);
     });
+    /****TOTLTIPS******/
+    $('[data-toggle="tooltip"]').tooltip()
 });
  
 
